fb1b2fe42da034225cc70fecdf32a368a154b2b2
pn lin <245810159@qq.com>
2020-03-04 14:10:09 +0800

    dev1
    dev2

---
+++ q
@@ -0,0 +1,67 @@
+[33mcommit 65204575d9d15bca2cafd573b45a50a430de2254[m[33m ([m[1;36mHEAD -> [m[1;32mdev[m[33m, [m[1;31morigin/dev[m[33m)[m
+Author: pn lin <245810159@qq.com>
+Date:   Wed Mar 4 14:10:48 2020 +0800
+
+    dev2
+
+[33mcommit fd67f3f4074a817f3e7437fdce5334621f236bae[m
+Author: pn lin <245810159@qq.com>
+Date:   Wed Mar 4 14:10:09 2020 +0800
+
+    dev1
+
+[33mcommit 37c75d8e4f860290a57fcdc0915d87f22098c649[m
+Author: pn lin <245810159@qq.com>
+Date:   Wed Mar 4 13:56:11 2020 +0800
+
+    路由和基础
+
+[33mcommit 0a116fd37a197f3d5061542516f1570857779712[m
+Author: pn lin <245810159@qq.com>
+Date:   Wed Mar 4 10:48:27 2020 +0800
+
+    dev
+    
+    eslintConfig
+
+[33mcommit 9c151d0d96e01a20fc16245b10a6dafd54fe233c[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Wed Jul 4 17:23:34 2018 +0800
+
+    更新
+
+[33mcommit 07b95d4ebf3cdb367b2d8c87c4c8bf6c865fefc9[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Thu Jun 7 16:01:58 2018 +0800
+
+    更新路由
+
+[33mcommit d6facb31a3e63cd0b332af0dabb1d0ce95d02011[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Mon Jun 4 17:33:38 2018 +0800
+
+    更新路由
+
+[33mcommit b31b988ebfc65b4c653b116b3b27856dee1a45d3[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Mon Jun 4 11:36:29 2018 +0800
+
+    更新
+
+[33mcommit 6d6f66086bfc2cbe32109fc3fa20b65e0a84df53[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Mon Jun 4 11:18:51 2018 +0800
+
+    添加vue2.x版本
+
+[33mcommit 53737418fcad45c968723ac7833c71f1603f5d6d[m[33m ([m[1;32mmaster[m[33m)[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Mon Jun 4 10:49:26 2018 +0800
+
+    修改readme
+
+[33mcommit c01c682019275b55084a36ed397bc5ad36504e92[m
+Author: PN-linshifeng <245810159@qq.com>
+Date:   Mon Jun 4 10:32:57 2018 +0800
+
+    Initial commit
--- src/App.vue
+++ src/App.vue
@@ -6,73 +6,72 @@
     </div>
     <div class="main-wrap">
       <transition :name="view">
-          <router-view class="view-page"></router-view>
+        <router-view class="view-page"></router-view>
       </transition>
-      
     </div>
   </div>
 </template>
 <script>
-  import Header from "@/components/header.vue";
-  import Aside from "@/components/Aside.vue";
+import Header from "@/components/header.vue";
+import Aside from "@/components/Aside.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Header,
     Aside
   },
-  data:function(){
-      return{
-        view:localStorage.getItem("routerTransiton")
-      }
-    },
-  watch:{
-    '$route'(){
-      this.view=localStorage.getItem("routerTransiton");
+  data: function() {
+    return {
+      view: localStorage.getItem("routerTransiton")
+    };
+  },
+  watch: {
+    $route() {
+      this.view = localStorage.getItem("routerTransiton");
     }
   }
-}
+};
 </script>
 <style lang="scss">
-html,body{
+html,
+body {
   margin: 0px;
   padding: 0px;
   line-height: 1.4;
-  height:100%;
+  height: 100%;
 }
-.has-topber{
-  padding-top:80px;
-  .aside-wrap{
-    top:80px;
+.has-topber {
+  padding-top: 80px;
+  .aside-wrap {
+    top: 80px;
   }
-  .header{
-    top:0px;
+  .header {
+    top: 0px;
   }
 }
-.has-aside{
+.has-aside {
   padding-left: 250px;
 }
 
-.aside-wrap{
+.aside-wrap {
   position: fixed;
-  left:0px;
+  left: 0px;
   bottom: 0px;
   z-index: 9;
   border-right: 1px solid #e2e2e2;
-  width:250px;
-  margin-right:20px;
-  background:#f0f0f0;
-  overflow:auto;
+  width: 250px;
+  margin-right: 20px;
+  background: #f0f0f0;
+  overflow: auto;
 }
-.main-wrap{
+.main-wrap {
   margin: 0px 50px;
-  height:100%;
+  height: 100%;
   position: relative;
 }
 
-
 #app {
-  height:100%;
+  height: 100%;
 }
 #nav {
   padding: 30px;
@@ -85,14 +84,18 @@ html,body{
   }
 }
 
-
-.view-left-enter,.view-right-leave-to{;
-  transform:translate(100%);
+.view-left-enter,
+.view-right-leave-to {
+  transform: translate(100%);
 }
-.view-left-leave-to,.view-right-enter{
-  transform:translate(-100%);
+.view-left-leave-to,
+.view-right-enter {
+  transform: translate(-100%);
 }
-.view-left-enter-active,.view-left-leave-active,.view-right-enter-active,.view-right-leave-active{
+.view-left-enter-active,
+.view-left-leave-active,
+.view-right-enter-active,
+.view-right-leave-active {
   transition: all 0.3s;
   position: absolute;
   width: 100%;
